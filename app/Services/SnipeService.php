a15bb5e60a65a58cd16044175bba8655ac2ea31a
Scott Limmer <s.limmer@federation.edu.au>
2023-09-08 15:51:46 +1000

    VUW-19

--- app/Services/SnipeService.php
+++ app/Services/SnipeService.php
@@ -21,27 +21,23 @@ class SnipeService
                 SELECT
                     MAX("count") AS "count",
                     "season"
-                FROM "snipe_all_site_data"
+                FROM "snipe_all_site_data_cleaned"
                 WHERE ST_CONTAINS(
-                    ST_GeomFromText(?),
-                    ST_POINT("long", "lat")
+                    ST_GeomFromText(?, 7844),
+                    ST_Transform("geom", 7844)
                 )
                 GROUP BY "season"
             )
             SELECT
-                DISTINCT snipe_all_site_data."season",
+                DISTINCT snipe_all_site_data_cleaned."season",
                 season_counts."count"
-            FROM snipe_all_site_data
-            LEFT JOIN season_counts ON snipe_all_site_data.season = season_counts."season"
+            FROM snipe_all_site_data_cleaned
+            LEFT JOIN season_counts ON snipe_all_site_data_cleaned.season = season_counts."season"
             WHERE
-                "eventDate" >= to_date('2014-07-01', 'YYYY-MM-DD') and "eventDate" <= to_date('2023-06-30', 'YYYY-MM-DD')
-            ORDER BY snipe_all_site_data."season"
+                "date" >= to_date('2014-07-01', 'YYYY-MM-DD') and "date" <= to_date('2023-06-30', 'YYYY-MM-DD')
+            ORDER BY snipe_all_site_data_cleaned."season"
     SQL
         );
-
-        // TODO: reimport snipe_all_site_date from csv
-        // TODO: create cleaned view to create geom
-        // TODO: update maxSeasonCount sql to use "geom" rather than st_point, specify 7844 on geomfromtext
 
         return DB::select($sql, [$wkt]);
     }
