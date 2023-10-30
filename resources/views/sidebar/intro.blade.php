cb656c99a49e5f5b793c8d210187899b62e6b7ee
Scott Limmer <s.limmer@federation.edu.au>
2023-10-30 15:25:09 +1100

    VUW-36 Update content from sharepoint doc

--- resources/views/sidebar/intro.blade.php
+++ resources/views/sidebar/intro.blade.php
@@ -5,12 +5,18 @@
 </h1>
 <h2>About</h2>
 <p>
-    This project uses citizen science waterbird observation data along with geographic information relevant to land use to demonstrate relative value of wetlands in the Greater Melbourne
+    This project uses citizen science waterbird observation data along with geographic information relevant to land use
+    to demonstrate relative value of wetlands in the Greater Melbourne
     region.
 </p>
-<p>Urban wetlands collectively support waterbirds and other biodiversity assets. These contribute to healthy "green spaces" in cities. Some individual wetlands will support a greater
-    diversity of species than others. But all wetlands are important and all have value as habitat for flora and fauna, for maintaining water quality, for helping with flood regulation and
+<p>Urban wetlands collectively support waterbirds and other biodiversity assets. These contribute to healthy "green
+    spaces" in cities. Some individual wetlands will support a greater
+    diversity of species than others. But all wetlands are important, and all have value as habitat for flora and fauna,
+    for maintaining water quality, for helping with flood regulation and
     for human health and wellbeing.</p>
+<p>
+    Use this tool to learn more about your local wetland.
+</p>
 <p>
     <a href="#about">Read more..</a>
 </p>
@@ -18,11 +24,11 @@
 <h2>How to use this tool</h2>
 <h3>To locate your wetland:</h3>
 <p>
-    Enter a wetland name or street address in the "Address search" box OR<br>
+    Enter a wetland name in the search box OR<br>
     Use the mouse to pan and zoom to the location
 </p>
 <p>
-    You can filter wetland types by Land use and by Protected area status
+    You can filter wetland types by Protected area status
 </p>
 
 <h3>
@@ -32,13 +38,42 @@
     Navigate to the wetland of interest and click on wetland to get a report<br>
     The report queries all citizen science records and geographic information within 350m of the wetland edge
 </p>
+<p>
+    <a href="https://inaturalist.ala.org.au/" target="_blank">Share your wetland fauna and flora observations with the Atlas of Living
+        Australia</a>
+</p>
 <h3>Partners</h3>
+<p>
+    This project was funded by the Australian Urban Research Infrastructure Network in partnership with the Atlas of
+    Living Australia. The project is led by the Centre for eResearch and Digital Innovation, Federation Australia.
+    BirdLife Australia, the University of Melbourne and Melbourne Water are project partners and have contributed data
+    and information to the project.
+</p>
 <div class="partners">
-    <a href="https://aurin.org.au/" target="_blank"><img src="{{ url('storage/partners/aurin-logo.png') }}" alt="Australian Urban Research Infrastructure Network (AURIN)"></a>
-    <a href="https://ala.org.au/" target="_blank"><img src="{{ url('storage/partners/ALA_Logo_Stacked_RGB.png') }}" alt="Atlas of Living Australia (ALA)"></a>
-    <a href="https://birdlife.org.au/" target="_blank"><img src="{{ url('storage/partners/BIRDLIFE-Logo.jpg') }}" alt="BirdLife Australia"></a>
-    <a href="https://www.unimelb.edu.au/" target="_blank"><img src="{{ url('storage/partners/uni-melbourne.svg') }}" alt="The University of Melbourne"></a>
-    <a href="https://www.cerdi.edu.au/" target="_blank"><img src="{{ url('storage/partners/cerdi.jpg') }}" alt="Centre for eResearch and Digital Innovation (CeRDI)"></a>
-    <a href="https://www.melbournewater.com.au/" target="_blank"><img src="{{ url('storage/partners/mw_logo_rgb.svg') }}" alt="Melbourne Water"></a>
+    <a href="https://aurin.org.au/" target="_blank">
+        <img src="{{ url('storage/partners/aurin-logo.png') }}"
+             alt="Australian Urban Research Infrastructure Network (AURIN)">
+    </a>
+    <a href="https://ala.org.au/" target="_blank">
+        <img src="{{ url('storage/partners/ALA_Logo_Stacked_RGB.png') }}"
+             alt="Atlas of Living Australia (ALA)">
+    </a>
+    <a href="https://birdlife.org.au/" target="_blank">
+        <img src="{{ url('storage/partners/BIRDLIFE-Logo.jpg') }}"
+             alt="BirdLife Australia">
+    </a>
+    <a href="https://www.unimelb.edu.au/" target="_blank">
+        <img src="{{ url('storage/partners/uni-melbourne.svg') }}"
+             alt="The University of Melbourne">
+    </a>
+    <a href="https://www.cerdi.edu.au/" target="_blank">
+        <img src="{{ url('storage/partners/cerdi.jpg') }}"
+             alt="Centre for eResearch and Digital Innovation (CeRDI)">
+    </a>
+    <a href="https://www.melbournewater.com.au/" target="_blank">
+        <img
+            src="{{ url('storage/partners/mw_logo_rgb.svg') }}"
+            alt="Melbourne Water">
+    </a>
 </div>
 
