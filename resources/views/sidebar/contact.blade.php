6dd8cee781a98c270e9cba7ffb7e8a623e1f6e7c
Scott Limmer <s.limmer@federation.edu.au>
2023-10-31 12:37:52 +1100

    VUW-36 Update content from sharepoint doc update contact info

--- resources/views/sidebar/contact.blade.php
+++ resources/views/sidebar/contact.blade.php
@@ -5,11 +5,11 @@
 </p>
 
 <p>
-    If you would like to learn more or get involved in Latham’s Snipe monitoring please <mark>contact Birgita</mark>.
+    If you would like to learn more or get involved in Latham's Snipe monitoring please <a href="https://www.cerdi.edu.au/team#birgita_hansen" target="_blank">contact Birgita</a>.
 </p>
 
 <p>
-    For technical support, please contact the <mark>CeRDI Helpdesk</mark>.
+    For technical support, please contact <a href="https://www.cerdi.edu.au/contactus" target="_blank">CeRDI</a>.
 </p>
 
 <p>
