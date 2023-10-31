ad337613bb4212f9dbedbf21287a624e47012f4c
Scott Limmer <s.limmer@federation.edu.au>
2023-10-31 14:57:26 +1100

    VUW-36 Update content from sharepoint doc

--- resources/views/sidebar/about.blade.php
+++ resources/views/sidebar/about.blade.php
@@ -41,26 +41,26 @@
     which is a cryptic migratory shorebird that breeds in northern Japan and eastern Russia during the austral winter.
     It migrates to Australia in late winter, and it spends its non-breeding season in shallow, well-vegetated freshwater
     and estuarine wetlands where it builds up energy reserves for its return migration in autumn. As with all migratory
-    shorebird species in the <a href="https://www.eaaflyway.net/migratory-waterbirds/">East Asian-Australasian
+    shorebird species in the <a href="https://www.eaaflyway.net/migratory-waterbirds/" target="_blank">East Asian-Australasian
         Flyway</a>, its survival depends upon access to a range of habitats across its migratory lifecycle. In the case
     of Latham's Snipe, these habitats can often be found in urban areas or in areas converted to agricultural use. The
     species is threatened primarily by wetland habitat loss across its non-breeding range.
 </p>
 <p>
     Latham's Snipe is one of 37 migratory shorebird species listed as matters of national environmental significance
-    under the <a href="https://www.dcceew.gov.au/environment/epbc">Environment Protection and Biodiversity Conservation
+    under the <a href="https://www.dcceew.gov.au/environment/epbc" target="_blank">Environment Protection and Biodiversity Conservation
         Act 1999 (EPBC Act)</a>. At the global level, it is listed as <a
-        href="https://www.iucnredlist.org/species/22693078/209318491">Near Threatened under the IUCN Red List </a> of
+        href="https://www.iucnredlist.org/species/22693078/209318491" target="_blank">Near Threatened under the IUCN Red List </a> of
     species (BirdLife International, 2023), in recognition that it is potentially a declining species, but that
     information is lacking to determine trends and guide a management response.
 </p>
 
 <p>
-    The <a href="https://lathamssnipeproject.wordpress.com/">Latham's Snipe Project</a> was established in 2015 to fill
+    The <a href="https://lathamssnipeproject.wordpress.com/" target="_blank">Latham's Snipe Project</a> was established in 2015 to fill
     critical knowledge gaps about the ecology and management of this species, to improve conservation outcomes. This is
     a national scale citizen science program that also includes amateur and professional ornithologists. A key finding
     of monitoring to date is relatively common occurrence of snipe in urban wetlands, often in numbers exceeding the <a
-        href="https://www.dcceew.gov.au/environment/epbc/publications/shorebirds-guidelines">EPBC Act threshold for
+        href="https://www.dcceew.gov.au/environment/epbc/publications/shorebirds-guidelines" target="_blank">EPBC Act threshold for
         national importance of 18 birds</a>. Many of these wetlands have no formal protection and every year, some get
     lost to land development and land use change.
 </p>
@@ -86,28 +86,28 @@
 </h2>
 
 <p>
-    The <a href="https://ala.org.au/">Atlas of Living Australia</a> is the primary aggregator and repository of citizen
+    The <a href="https://ala.org.au/" target="_blank">Atlas of Living Australia</a> is the primary aggregator and repository of citizen
     science data in Australia. Read about Dr Erin Roger's review of ALA data contributions by citizen scientists <a
-        href="https://www.csiro.au/en/news/All/News/2023/September/Aussies-provide-more-than-62-million-species-records-for-conservation-research">
+        href="https://www.csiro.au/en/news/All/News/2023/September/Aussies-provide-more-than-62-million-species-records-for-conservation-research" target="_blank">
         Read about citizen science records in the ALA</a>
 
 
 <p>
-    Find out about BirdLife Australia's <a href="https://birdlife.org.au/programs/urban-birds/">Urban birds program</a>
+    Find out about BirdLife Australia's <a href="https://birdlife.org.au/programs/urban-birds/" target="_blank">Urban birds program</a>
     and
-    their <a href="https://birdlife.org.au/programs/wetland-birds/">Wetland birds program</a>
+    their <a href="https://birdlife.org.au/programs/wetland-birds/" target="_blank">Wetland birds program</a>
 
 <p>
-    The University of Melbourne led the establishment of the <a href="https://nespurban.edu.au/">Clean Air and Urban
+    The University of Melbourne led the establishment of the <a href="https://nespurban.edu.au/" target="_blank">Clean Air and Urban
         Landscapes Hub</a>. Read their reports on <a
-        href="https://nespurban.edu.au/wp-content/uploads/2019/06/Actions-for-Biodiversity-PART-I.pdf">conservation of
+        href="https://nespurban.edu.au/wp-content/uploads/2019/06/Actions-for-Biodiversity-PART-I.pdf" target="_blank">conservation of
         urban biodiversity</a> and <a
-        href="https://nespurban.edu.au/wp-content/uploads/2020/10/Recognising-the-conservation-and-cultural-value-of-urban-wetlands.pdf">conservation
+        href="https://nespurban.edu.au/wp-content/uploads/2020/10/Recognising-the-conservation-and-cultural-value-of-urban-wetlands.pdf" target="_blank">conservation
         and cultural value of urban wetlands</a>.
 </p>
 
 <p>
-    <a href="https://www.sciencedaily.com/releases/2022/08/220816093620.htm">Decreasing backyard bird diversity</a>
+    <a href="https://www.sciencedaily.com/releases/2022/08/220816093620.htm" target="_blank">Decreasing backyard bird diversity</a>
     Science Daily, August 2022
 </p>
 
@@ -117,7 +117,7 @@
 
 <p>
     Urban plans can be amended to accommodate nature: read in the Conversation an article by <a
-        href=https://theconversation.com/what-if-urban-plans-gave-natural-systems-the-space-to-recover-from-the-cities-built-over-them-it-can-be-done-199388>
+        href="https://theconversation.com/what-if-urban-plans-gave-natural-systems-the-space-to-recover-from-the-cities-built-over-them-it-can-be-done-199388" target="_blank">
         Louise Wright and Catherine Murphy</a>
 </p>
 
@@ -144,17 +144,17 @@
 </p>
 <ul>
     <li>
-        <a href="https://ebird.org/">ebird</a> or
+        <a href="https://ebird.org/" target="_blank">ebird</a> or
     </li>
     <li>
-        <a href="https://birdata.birdlife.org.au/home">Birdata</a>
+        <a href="https://birdata.birdlife.org.au/home" target="_blank">Birdata</a>
     </li>
 </ul>
 
 <p>
-    You can share recordings of calling frogs using <a href="https://www.frogid.net.au/">FrogID</a>
+    You can share recordings of calling frogs using <a href="https://www.frogid.net.au/" target="_blank">FrogID</a>
 </p>
 
 <p>
-    You can lodge records other fauna and flora sightings with <a href="https://inaturalist.ala.org.au/">iNaturalist</a>
+    You can lodge records other fauna and flora sightings with <a href="https://inaturalist.ala.org.au/" target="_blank">iNaturalist</a>
 </p>
--- resources/views/sidebar/terms.blade.php
+++ resources/views/sidebar/terms.blade.php
@@ -11,12 +11,12 @@
 
 <p>
     This site is not a formal planning tool and developers seeking advice on specific wetland assets should consult the
-    Victorian State Government's <a href="https://vba.biodiversity.vic.gov.au/">Victorian Biodiversity Atlas</a> and
+    Victorian State Government's <a href="https://vba.biodiversity.vic.gov.au/" target="_blank">Victorian Biodiversity Atlas</a> and
     consult their local council, or seek a biodiversity assessment by a qualified consultant.
 </p>
 <p>
     Please be aware that any wetland site supporting 18 or more Latham's Snipe should be assessed in relation to the <a
-        href="https://www.dcceew.gov.au/environment/epbc/publications/shorebirds-guidelines">EPBC Act Significant Impact
+        href="https://www.dcceew.gov.au/environment/epbc/publications/shorebirds-guidelines" target="_blank">EPBC Act Significant Impact
         Guidelines</a>. Any proposal for land development that may impact a wetland used by Latham's Snipe may need to
     be referred to the Commonwealth under the EPBC Act.
 </p>
