c62384821c4331e3428757b622c57d5018ffddc8
rsinha <r.sinha@federation.edu.au>
2024-03-14 13:01:14 +1100

    CH-4962 final feedback changes 2

--- resources/js/components/SpeciesList.vue
+++ resources/js/components/SpeciesList.vue
@@ -55,7 +55,7 @@ export default {
 <template>
     <div>
         <div :id="id" class="modal fade" tabindex="-1">
-            <div class="modal-dialog modal-xl modal-dialog-centered">
+            <div class="modal-dialog modal-xl">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title">{{ title }}</h5>
